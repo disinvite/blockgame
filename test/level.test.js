e5311448c8617d4496ba660ad1496c7ec5d5c160
disinvite <disinvite@users.noreply.github.com>
2019-10-21 16:46:51 -0400

    testing framework?

--- test/level.test.js
+++ test/level.test.js
@@ -1,41 +1,27 @@
 const assert = require('assert');
 const { LEVEL } = require('../lib/level');
-
-const l1 = `
-$ .. 
-....=
-.... 
- ..  
-     
-`;
-
-// after step down
-
-const l2 = `
-. .. 
-$...=
-.... 
- ..  
-     
-`;
-
-const no_movement = `
-$.
- .
-`;
+const testcase = require('./testcase');
 
 describe('steps', () => {
   it('should work for this simple case', () => {
-    const x = new LEVEL(l1);
+    const x = new LEVEL(testcase.step_down_simple_before);
     x.stepDown()
-    const y = new LEVEL(l2);
+    const y = new LEVEL(testcase.step_down_simple_after);
     assert.ok(x.toString() === y.toString());
   });
 
   it('should work for a case where there is nothing to move', () => {
-    const x = new LEVEL(no_movement);
+    const x = new LEVEL(testcase.step_down_cant_move);
     x.stepDown();
-    const y = new LEVEL(no_movement);
+    const y = new LEVEL(testcase.step_down_cant_move);
     assert.ok(x.toString() === y.toString());
   });
+
+  it('should work for a stack of items', () => {
+    const x = new LEVEL(testcase.step_down_two_items_before);
+    x.stepDown()
+    const y = new LEVEL(testcase.step_down_two_items_after);
+    assert.ok(x.toString() === y.toString());
+  });
+
 });
---
+++ test/testcase.js
@@ -0,0 +1,35 @@
+const step_down_simple_before = `
+$.
+..
+`;
+
+const step_down_simple_after = `
+..
+$.
+`;
+
+const step_down_cant_move = `
+$.
+ .
+`;
+
+const step_down_two_items_before = `
+$..
+$..
+...
+`;
+
+const step_down_two_items_after = `
+...
+$..
+$..
+`;
+
+module.exports = {
+ step_down_simple_before,
+ step_down_simple_after,
+ step_down_cant_move,
+ step_down_two_items_before,
+ step_down_two_items_after
+}
+
