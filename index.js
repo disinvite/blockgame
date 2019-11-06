c7c522a52e10587b2ac4710345d83c16fda2ee19
disinvite <disinvite@users.noreply.github.com>
2019-11-06 15:58:23 -0500

    updated testing code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { LEVEL } = require('./lib/level');
+const { solve } = require('./lib/solver');
 const testcase = require('./test/testcase');
 
 const l1 = `
@@ -19,11 +20,28 @@ $...=
      
 `;
 
+const s = [
+  ' . A^',
+  '..AA ',
+  '.C$. ',
+  '.C.. ',
+  'B    '
+];
+
+const xlevel = new LEVEL(s.join('\n'));
+//console.log(solve(xlevel, 10));
+console.log(`\nBEFORE:\n${xlevel}`);
+
+// some nice ascii there.
+console.log(xlevel.metasprites.map(x => x.map(y => y.padEnd(6, ' ')).join(' | ')).join('\n'));
+
 //const x = new LEVEL(l1);
 //console.log(x.tiles);
 
-const x = new LEVEL(testcase.stacked_shapes_try_to_break_it);
+/*
+const x = new LEVEL(testcase.two_shapes_before);
 console.log(`\nBEFORE:\n${x}`);
 x.stepDown();
 
 console.log(`\nAFTER:\n${x}`);
+*/
