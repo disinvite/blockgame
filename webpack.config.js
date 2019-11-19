452aa5a825342bbdc44c99e352fe6f8bb999e375
disinvite <disinvite@users.noreply.github.com>
2019-11-18 19:43:43 -0500

    dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 9000
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'app/viewer.html')
+    })
+  ],
   module: {
     rules: [
       {
